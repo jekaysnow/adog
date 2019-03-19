af0555e83e94da51ce3be80de944316d3cea10fa
jekaysnow <164358445@qq.com>
2019-03-20 00:43:01 +0800

    init project

---
+++ adog-server/src/main/java/vip/adog/AdogServerApplication.java
@@ -0,0 +1,13 @@
+package vip.adog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdogServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdogServerApplication.class, args);
+	}
+
+}
