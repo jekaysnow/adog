d29bd7b9521eab953a9b8472d403677b6f06d11c
jekaysnow <164358445@qq.com>
2019-03-25 22:53:13 +0800

    add user-service

---
+++ user-service/src/main/java/vip/adog/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package vip.adog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/main/java/vip/adog/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package vip.adog.service.impl;
+
+/**
+ * Created by jekay on 19-3-25.
+ */
+public class UserServiceImpl {
+}
